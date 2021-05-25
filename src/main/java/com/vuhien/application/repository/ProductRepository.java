bae2b36ab2e7d2c3f6060efbcc3083266618609d
hienvv <vuhien96.jr@gmail.com>
2021-05-25 10:37:21 +0700

    get product admin

--- src/main/java/com/vuhien/application/repository/ProductRepository.java
+++ src/main/java/com/vuhien/application/repository/ProductRepository.java
@@ -22,13 +22,13 @@ public interface ProductRepository extends JpaRepository<Product, String> {
     Product findByName(String name);
 
     //Lấy tất cả sản phẩm
-    @Query(value = "SELECT * FROM product p " +
+    @Query(value = "SELECT * FROM product pro right join (SELECT DISTINCT p.* FROM product p " +
             "INNER JOIN product_category pc ON p.id = pc.product_id " +
             "INNER JOIN category c ON c.id = pc.category_id " +
             "WHERE p.id LIKE CONCAT('%',?1,'%') " +
             "AND p.name LIKE CONCAT('%',?2,'%') " +
             "AND c.id LIKE CONCAT('%',?3,'%') " +
-            "AND p.brand_id LIKE CONCAT('%',?4,'%')", nativeQuery = true)
+            "AND p.brand_id LIKE CONCAT('%',?4,'%')) as tb1 on pro.id=tb1.id", nativeQuery = true)
     Page<Product> adminGetListProducts(String id, String name, String category, String brand, Pageable pageable);
 
 //    @Query(value = "SELECT NEW com.vuhien.application.model.dto.ProductInfoDTO(p.id, p.name, p.slug, p.price ,p.images ->> '$[0]', p.total_sold) " +
